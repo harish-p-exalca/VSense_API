64ee71b978daf4eaa7a8e50d1475fae09f35d20e
harish-p-exalca <harish.p@exalca.com>
2021-06-15 10:35:12 +0530

    Rule Updated june14

--- vsenseapi-main/Migrations/20210614061906_Rule updated.Designer.cs
+++ vsenseapi-main/Migrations/20210614061906_Rule updated.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VSense.API.Context;
 
 namespace VSense.API.Migrations
 {
     [DbContext(typeof(DeviceContext))]
-    partial class DeviceContextModelSnapshot : ModelSnapshot
+    [Migration("20210614061906_Rule updated")]
+    partial class Ruleupdated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ vsenseapi-main/Migrations/20210614061906_Rule updated.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VSense.API.Migrations
+{
+    public partial class Ruleupdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Rule",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Rule");
+        }
+    }
+}
